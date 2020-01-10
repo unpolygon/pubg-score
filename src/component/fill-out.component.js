e97603441296abe1f8ca0c8692cb627151da3bf5
Unpolygon <saengsetth@outlook.com>
2020-01-11 01:17:33 +0700

    add function showScore

--- src/component/fill-out.component.js
+++ src/component/fill-out.component.js
@@ -2,24 +2,6 @@ import React from 'react';
 import { db } from '../firebase';
 
 export default function fillOut(){
-    const renderData = () => {
-        let doc = {id:'parn', name: 'Hello', city: 'world'};
-        const fillOutList = document.querySelector('#fillOut');
-
-        let li = document.createElement('li');
-        let name = document.createElement('span');
-        let city = document.createElement('span');
-    
-        li.setAttribute('data-id', doc.id);
-        name.textContent = doc.name;
-        city.textContent = doc.city;
-    
-        li.appendChild(name);
-        li.appendChild(city);
-        console.log(fillOutList)
-        if (fillOutList !== null) fillOutList.appendChild(li);
-    }
-
     return(
         <ul id="fillOut">
             <li>
@@ -27,7 +9,7 @@ export default function fillOut(){
                 <input placeholder="Your name.."></input>
                 <input placeholder="First kill.."></input>
                 <input placeholder="Second kill.."></input>
-                <button onClick ={renderData()}>Submit</button>
+                <button>Submit</button>
             </li>
         </ul>
     );
--- src/component/show-score.component.js
+++ src/component/show-score.component.js
@@ -1,10 +1,60 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 
 export default function showScore(){
+    useEffect(()=>{
+        renderData;
+    });
+
+    const renderData = () => {
+        let doc = {
+            id: 'iaun',
+            firstName: 'Parn',
+            secondName: 'Milly', 
+            firstKill: 0, 
+            secondKill: 0 , 
+            score: 0
+        };
+        const teamList = document.querySelector('#team');
+
+        let li = document.createElement('li');
+        let teamName = document.createElement('span');
+        let firstName = document.createElement('span');
+        let secondName = document.createElement('span');
+        let firstKill = document.createElement('span');
+        let secondKll = document.createElement('span');
+        let score = document.createElement('span');
+        
+        firstName.className='firstName';
+        secondName.className='secondName';
+        firstKill.className='firstKill';
+        secondKll.className='secondKll';
+        score.className='score';
+        
+        li.setAttribute('data-id', doc.id);
+        teamName.textContent=doc.id;
+        firstName.textContent=doc.firstName;
+        secondName.textContent=doc.secondName;
+        firstKill.textContent=doc.firstKill;
+        secondKll.textContent=doc.secondKll;
+        score.textContent=doc.score;
+    
+        li.appendChild(teamName);
+        li.appendChild(firstName);
+        li.appendChild(secondName);
+        li.appendChild(firstKill);
+        li.appendChild(secondKll);
+        li.appendChild(score);
+
+        if (teamList !== null) teamList.appendChild(li);
+    }
     return(
         <div className='showScore'>
-            showscore
+            <button onClick ={renderData}>Show</button>
+            <ul id ='team'>
+                <li className='Head'>
+                    <p></p>
+                </li>
+            </ul>
         </div>
     );
 }
