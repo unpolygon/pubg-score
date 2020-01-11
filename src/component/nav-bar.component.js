cf6c04a893f23cf7e96e6c4267365a94706493eb
Unpolygon <saengsetth@outlook.com>
2020-01-11 19:42:18 +0700

    focus button navbar

--- src/component/nav-bar.component.js
+++ src/component/nav-bar.component.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './nav-bar.component.css';
+import $ from 'jquery';
 
 export default function navBar(){
+    const handleClick = (e) => {
+        $('a').focus(function(){
+            $(this).addClass('active');
+        });
+        $('a').blur(function(){
+            $(this).removeClass('active');
+        })
+    }
+
     return(
         <div className='navBar'>
-            <Link to="/" className="navBar-Link">Fill Out</Link>
-            <Link to="/Edit" className="navBar-Link">Show Score</Link>
-            <Link to="/Create" className="navBar-Link">Create</Link>
+            <Link to="/" className="navBar-Link" id='fill-out' onClick={handleClick}>Fill Out</Link>
+            <Link to="/Edit" className="navBar-Link" id='show-score' onClick={handleClick}>Show Score</Link>
+            <Link to="/Create" className="navBar-Link" id='create' onClick={handleClick}>Create</Link>
         </div>
     );
 }
