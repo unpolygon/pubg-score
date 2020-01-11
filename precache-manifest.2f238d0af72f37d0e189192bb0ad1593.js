fb5fc39ed781255090b384a6fae92f432a90f2ac
Unpolygon <saengsetth@outlook.com>
2020-01-11 18:25:21 +0700

    Updates

---
+++ precache-manifest.2f238d0af72f37d0e189192bb0ad1593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3a76404ce477720aa0e3f3c5331b87",
+    "url": "/pubg-score/index.html"
+  },
+  {
+    "revision": "16c88f8b8154d16abbf6",
+    "url": "/pubg-score/static/css/main.9ef63ef7.chunk.css"
+  },
+  {
+    "revision": "7ef7a24d73b1f3fa0864",
+    "url": "/pubg-score/static/js/2.9296d10a.chunk.js"
+  },
+  {
+    "revision": "f73e810844fba009bd106bf78cf93a52",
+    "url": "/pubg-score/static/js/2.9296d10a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16c88f8b8154d16abbf6",
+    "url": "/pubg-score/static/js/main.46e70e50.chunk.js"
+  },
+  {
+    "revision": "f5419980447c8fb06c88",
+    "url": "/pubg-score/static/js/runtime-main.a46144a2.js"
+  }
+]);
